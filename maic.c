fcd086e99408d1752aae328f7cbe8abc79b6f2c4
gadas <gadas4415@gmail.com>
2024-01-12 18:31:58 +0700

    end

--- maic.c
+++ maic.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
-void glaslatin(char arr[], *res[], int size);
+void glaslatin(char arr[], char res[], int size);
 
 
-int main(int argc, char const *argv[])
+int main()
 {
-    char arr[] = "abcdefghijklmnopqrstuvwxyz";
+    char arr[] = "fijajnsdilf;ouqiuhwfiuonb 3123iufbaspdifu";
     int size = sizeof(arr);
     char res[size] = {};
-    glaslatin(arr, *res, size);
+    glaslatin(arr, res, size);
     for (int i = 0; i < size; i++)
-    {
-        printf("%c ", res[i]);
+    {   
+        if (res[i] != 0){
+            printf("%c ", res[i]);
+        }
     }
-    
+    printf("\n");
     return 0;
 }
 
-void glaslatin(char arr[], char *res[], int size){
+void glaslatin(char arr[], char res[], int size){
     for (int i = 0, j = 0; i < size; i++)
     {
         if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u' || arr[i] == 'y'){
-            *res[j] = arr[i];
+            res[j] = arr[i];
             j++;
         }
     }
